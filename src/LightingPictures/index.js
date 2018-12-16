4873dca6938ca0c27eac11521404a40cee454287
Filip Tatarnik <ftatarnik@gmail.com>
2018-12-16 17:02:18 -0600

    Added navbar component. Added the LightingPictures component that'll be the content underneath the navbar and the main content for the site.

---
+++ src/LightingPictures/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class LightingPicture extends Component {
+    render(){
+        return(
+      <h1>This is where the pictures that Watts is going to give us are going to go.</h1>
+        )
+    }
+}
+export default LightingPicture;
