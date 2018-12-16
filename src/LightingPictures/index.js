bfea07f2651eedcc307d9ec508554031a48ef12a
Filip Tatarnik <ftatarnik@gmail.com>
2018-12-16 17:19:37 -0600

    put the img in a card that'll eventually hold what will be the carusel

--- src/LightingPictures/index.js
+++ src/LightingPictures/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
 
 
 class LightingPicture extends Component {
@@ -6,8 +7,10 @@ class LightingPicture extends Component {
         return(
     <div>
       <h1>This is where the pictures that Watts is going to give us are going to go.</h1>
-      <img src="https://i.imgur.com/I1wluQE.jpg">
-      </img>
+      <Card style={{maxHeight: 750, maxWidth: 750, marginLeft: 200}}>
+      	<img src="https://i.imgur.com/I1wluQE.jpg">
+      	</img>
+      </Card>
     </div>
         )
     }
