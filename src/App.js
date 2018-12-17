45327f40e310ff359d7c211cb65b26c5f2a68e80
Filip Tatarnik <ftatarnik@gmail.com>
2018-12-16 22:59:17 -0600

    started with react-router-transitions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ButtonAppBar from './ButtonAppBar';
 import LightingPictures from './LightingPictures';
 import LabelBottomNavigation from './LabelBottomNavigation';
 import {Route, Switch} from 'react-router-dom'
+import { AnimatedSwitch } from 'react-router-transition';
 import AboutContainer from './AboutContainer'
 import Album from './Album';
 import LightsContainer from './LightsContainer'
@@ -18,12 +19,17 @@ class App extends Component {
       <div className="App">
         <ButtonAppBar />
         
-        <Switch>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+          >
           <Route exact path="/about" component={AboutContainer}/>
           <Route exact path="/lights" component={LightsContainer}/>
           <Route exact path="/speakers" component={SpeakerContainer}/>
           <Route exact path="/home" component={LightingPictures}/>
-        </Switch>
+        </AnimatedSwitch>
         <Album />
         <Divider />
         <LabelBottomNavigation />
