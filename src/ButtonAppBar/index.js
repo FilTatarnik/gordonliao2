1a9a71185db9a5b448ea8bc991169e810d148d42
Josh Hepworth <jrhepworth12@gmail.com>
2018-12-16 18:27:11 -0600

    foxed mav color

--- src/ButtonAppBar/index.js
+++ src/ButtonAppBar/index.js
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom'
+import './styles.css'
 
 const styles = {
   root: {
