7406722335082cdeb7924e8be66d0e65437c7b36
Filip Tatarnik <ftatarnik@gmail.com>
2018-12-17 13:45:03 -0600

    got all the components on the same page. Unfortuneatly i used br's to get the spaces for the moment. Next; autoscroll

--- src/LightingPictures/index.js
+++ src/LightingPictures/index.js
@@ -9,17 +9,17 @@ import './styles.css'
 class LightingPicture extends Component {
     render(){
         return(
-    <div class="back">
-    <br />
-      <h1 class="motivate">HELP OTHERS ACHIEVE THEIR DREAMS </h1>
-      <h1 class="motivate"> AND YOU WILL ACHIEVE YOURS.</h1>
-        <Divider />
-      	<br />
-      <br />
-      	<Divider />
-      <br />
-        <SimpleModal open={this.props.open} />
-    </div>
+          <div class="back">
+            <br />
+              <h1 class="motivate">HELP OTHERS ACHIEVE THEIR DREAMS </h1>
+              <h1 class="motivate"> AND YOU WILL ACHIEVE YOURS.</h1>
+                <Divider />
+              	<br />
+                <br />
+              	<Divider />
+                <br />
+                <SimpleModal open={this.props.open} />
+          </div>
         )
     }
 }
