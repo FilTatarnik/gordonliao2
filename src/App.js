c9f0de048ad74512f2af5bfb8f15f4a92903203e
Filip Tatarnik <ftatarnik@gmail.com>
2018-12-17 16:49:20 -0600

    took out react-scroll-to-component, trying to integrate react-scroll. It kind of works, all the buttons bring you to the top of the page at the moment

--- src/App.js
+++ src/App.js
@@ -5,13 +5,70 @@ import AboutContainer from './AboutContainer'
 import Album from './Album';
 import LightsContainer from './LightsContainer'
 import SpeakerContainer from './SpeakerContainer'
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import './App.css';
 
+
+
 class App extends Component {
+  constructor (props){
+      super(props);
+      // this.scrollToTop = this.scrollToTop.bind(this);
+  }
+   componentDidMount() {
+     
+    Events.scrollEvent.register('begin', function() {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register('end', function() {
+      console.log("end", arguments);
+    });
+
+  }
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+  scrollTo() {
+    scroller.scrollTo('lights', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
+  scrollToWithContainer() {
+
+    let goToContainer = new Promise((resolve, reject) => {
+
+      Events.scrollEvent.register('end', () => {
+        resolve();
+        Events.scrollEvent.remove('end');
+      });
+
+      scroller.scrollTo('home', {
+        duration: 5000,
+        delay: 0
+      });
+
+    });
+
+    goToContainer.then(() =>  
+        scroller.scrollTo('scroll-container-second-element', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart',
+            containerId: 'scroll-container'
+        }));
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
   render() {
     return (
       <div className="App">
-          <ButtonAppBar onClick={this.props.scrollToComponent}/>
+          <ButtonAppBar  onCLick={scroll.scrollTo('home')}/>
           <br />
           <br />
           <br />
@@ -24,39 +81,7 @@ class App extends Component {
           <br />
           <br />
           <br />
-          <LightingPictures onClick={this.props.scrollToComponent}/>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <Album />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <AboutContainer onClick={this.props.scrollToComponent}/>
+          <LightingPictures onCLick={scroll.scrollTo('home')}/>
           <br />
           <br />
           <br />
@@ -88,7 +113,39 @@ class App extends Component {
           <br />
           <br />
           <br />
-          <LightsContainer onClick={this.props.scrollToComponent}/>
+          <AboutContainer onCLick={scroll.scrollTo('about')}/>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Album />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />          
+          <LightsContainer onCLick={scroll.scrollTo('lights')}/>
           <br />
           <br />
           <br />
@@ -120,7 +177,7 @@ class App extends Component {
           <br />
           <br />
           <br />
-          <SpeakerContainer onClick={this.props.scrollToComponent}/>
+          <SpeakerContainer onCLick={scroll.scrollTo('speaker')}/>
           <br />
           <br />
           <br />
