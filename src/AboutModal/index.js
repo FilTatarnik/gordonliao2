9eb18fa2b185380c72b95d3cd7898ebc5aa95e58
Josh Hepworth <jrhepworth12@gmail.com>
2019-05-24 13:41:34 -0500

    added modal for team component

---
+++ src/AboutModal/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+import './styles.css'
+
+class ModalPage extends Component {
+state={
+  modal5: false
+}
+
+toggle = nr => () => {
+  let modalNumber = 'modal' + nr
+  this.setState({
+    [modalNumber]: !this.state[modalNumber]
+  });
+}
+
+render() {
+  return (
+    <MDBContainer>
+      <MDBBtn className="button" color="black" onClick={this.toggle(5)}>Learn more</MDBBtn>
+      <MDBModal isOpen={this.state.modal5} toggle={this.toggle(5)} size="large">
+        <MDBModalHeader toggle={this.toggle(5)}><h1><strong>Gordon C.C. Liao</strong></h1></MDBModalHeader>
+        <MDBModalBody className="modal-font">
+        <h4>
+         <strong>Prior to his current activities, Gordon spent 15 years in institutional Private Equity as a Principal and Investment professional with Baird Capital, Reliant Equity Investors, and Wind Point Partners. Gordon received his MBA from Harvard Business School where he was a Toigo Fellow and the Founder of the Asian American Business Association (AABA), and his BS from the University of Illinois, Champaign-Urbana, where he graduated with Highest Honors and as a recipient of the Bronze Tablet. </strong>
+         <br/>
+         <br/>
+         <strong>Gordon's core mission in life is focused on lifting up underserved communities, with an emphasis on economic wealth creation and the enhancement of self-esteem through the performing arts.  He is the past President of the Goodman Theater's Auxiliary Board, Founding Board Member and current Vice Chair of AAAIM (the Association of Asian American Investment Managers), and former Board member of the Chicago Sinfonietta. Finally, he is a published poet, produced playwright, spoken word artist and wedding singer.
+</strong>
+        </h4>
+        </MDBModalBody>
+        <MDBModalFooter>
+          <MDBBtn color="black" onClick={this.toggle(5)}>Close</MDBBtn>
+        </MDBModalFooter>
+      </MDBModal>
+    </MDBContainer>
+    );
+  }
+}
+
+export default ModalPage;
--- src/TeamComponent/index.js
+++ src/TeamComponent/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {  MDBRow, MDBCol, MDBCard, MDBAvatar, MDBCardBody, MDBIcon } from "mdbreact";
+import AboutModal from '../AboutModal'
 import './styles.css';
 
 
@@ -20,10 +21,9 @@ const TeamPage = () => {
               <p className="text-uppercase blue-text">Founder & Partner</p>
               <p className="grey-text">
                   A life-long resident of the Chicago area (originally from the far Southside), Gordon is an independent entrepreneur, investor, operator and performer.  He is a Co-founder and Partner of investment firms Promise Holdings LLC, Modjule LLC, Seismic CPG LLC and 4C Capital LLC, also founding and serving in operational roles in both control buyouts and early stage start-ups, primarily in the Consumer space. Among his companies where he is a co-owner and active board member/observer of, include Hickory Farms, Gateway Newsstands, SwingKing, HairClinical and AKIRA.
+                  <AboutModal />
 
-                  Prior to his current activities, Gordon spent 15 years in institutional Private Equity as a Principal and Investment professional with Baird Capital, Reliant Equity Investors, and Wind Point Partners. Gordon received his MBA from Harvard Business School where he was a Toigo Fellow and the Founder of the Asian American Business Association (AABA), and his BS from the University of Illinois, Champaign-Urbana, where he graduated with Highest Honors and as a recipient of the Bronze Tablet. 
-
-                  Gordon's core mission in life is focused on lifting up underserved communities, with an emphasis on economic wealth creation and the enhancement of self-esteem through the performing arts.  He is the past President of the Goodman Theater's Auxiliary Board, Founding Board Member and current Vice Chair of AAAIM (the Association of Asian American Investment Managers), and former Board member of the Chicago Sinfonietta. Finally, he is a published poet, produced playwright, spoken word artist and wedding singer.
+              
               </p>
               <ul className="list-unstyled mb-0">
                 <a href="#!" className="p-2 fa-lg">
