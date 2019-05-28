b9afe23d3eefcc75ef1d75e44ee86fddda38f6b0
Josh Hepworth <jrhepworth12@gmail.com>
2019-05-27 22:53:25 -0500

    added margin in about to prevent overlap

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBMask, MDBView, MDBCardTitle, MDBBtn, MDBIcon } from 'mdbreact';
 import { Link, animateScroll as scroll } from "react-scroll";
+import "./styles.css"
 
 class FullPageIntroWithFixedTransparentNavbar extends React.Component {
   constructor(props) {
@@ -104,10 +105,10 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
 
           <MDBView src="https://mdbootstrap.com/img/Photos/Others/gradient1.jpg">
             <MDBMask className="flex-center flex-column text-white text-center">
-                     <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">“Promise Holdings was created to help small companies reach their promise or potential, while always living and adding value with trust & integrity. That is our promise”</MDBCardTitle>
+                     <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold"><h2 className="topper">“Promise Holdings was created to help small companies reach their promise or potential, while always living and adding value with trust & integrity. That is our promise”</h2></MDBCardTitle>
                 <p className="mx-5 mb-5">Promise Holdings is a private investment firm focused on investing in small businesses, primarily in the consumer sector, with a special emphasis in food, fashion & beauty. We are not a Private Equity or Venture Capital firm so do not look to sell within 3 to 5 years. Instead we have a very long-term hold orientation and may never sell in certain instances. We also have a very flexible mandate as we invest in both late stage, mature businesses, as well as early stage start-ups. <strong>We can also invest a controlling stake, or a small, minority, non-control stake in businesses.  The only things we look for are:</strong></p><h3>Great people with high integrity, openness and character</h3><br/><h3>Fun people because we like to have a good time</h3><br/><h3>Good ideas</h3>
                 <br/>
-                  <li className="nav-item">
+                  
               <Link
                 activeClass="active"
                 to="section3"
@@ -118,7 +119,7 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
               >
                 <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> Learn more</MDBBtn>
                  </Link>
-            </li>
+            
             </MDBMask>
           </MDBView>
         </header>
