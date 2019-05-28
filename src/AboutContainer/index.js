e5345e52320e7619ef4f3da140426651b40c8383
Josh Hepworth <jrhepworth12@gmail.com>
2019-05-28 14:27:02 -0500

    changed p to h4

--- src/AboutContainer/index.js
+++ src/AboutContainer/index.js
@@ -17,8 +17,8 @@ const JumbotronPage = () => {
                 backgroundPosition: 'center',
               backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)`  
               }} id="about-image">
-              <MDBCol className="py-5">
-                <p className="mx-5 mb-5"><h4>“Promise Holdings was created to help small companies reach their promise or potential, while always living and adding value with trust & integrity. That is our promise”</h4></p>
+              <MDBCol className="py-5" id="about-text">
+                <h4 className="mx-5 mb-5">“Promise Holdings was created to help small companies reach their promise or potential, while always living and adding value with trust & integrity. That is our promise”</h4>
                 <p className="mx-5 mb-5">Promise Holdings is a private investment firm focused on investing in small businesses, primarily in the consumer sector, with a special emphasis in food, fashion & beauty. We are not a Private Equity or Venture Capital firm so do not look to sell within 3 to 5 years. Instead we have a very long-term hold orientation and may never sell in certain instances. We also have a very flexible mandate as we invest in both late stage, mature businesses, as well as early stage start-ups. <strong>We can also invest a controlling stake, or a small, minority, non-control stake in businesses.  The only things we look for are:</strong></p><h4>Great people with high integrity, openness and character</h4><br/><h4>Fun people because we like to have a good time</h4><br/><h4>Good ideas</h4>
                 <br/>
                 
