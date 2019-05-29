ad6de80ba1dd26632174b07d418b0b643bee52b5
Filip Tatarnik <ftatarnik@gmail.com>
2019-05-29 12:19:31 -0500

    removed the paragraph

--- src/AboutContainer/index.js
+++ src/AboutContainer/index.js
@@ -17,9 +17,8 @@ const JumbotronPage = () => {
                 <p className="mx-5 mb-5"></p><h3>“Promise Holdings was created to help small companies reach their promise or potential, while always living and adding value with trust & integrity. That is our promise”</h3>
                 <br />
                 <br/>
-                <p className="mx-5 mb-5">Promise Holdings is a private investment firm focused on investing in small businesses, primarily in the consumer sector, with a special emphasis in food, fashion & beauty. We are not a Private Equity or Venture Capital firm so do not look to sell within 3 to 5 years. Instead we have a very long-term hold orientation and may never sell in certain instances. We also have a very flexible mandate as we invest in both late stage, mature businesses, as well as early stage start-ups. <strong>We can also invest a controlling stake, or a small, minority, non-control stake in businesses.  The only things we look for are:</strong>
+
                 <br />
-                </p>
                 </MDBCol>
                 </MDBCol>
 
