e7330485918290558272dbd5034d5bae68b52a7e
Filip Tatarnik <ftatarnik@gmail.com>
2019-06-01 22:25:26 -0500

    pushing to deploy

--- src/AboutContainer/index.js
+++ src/AboutContainer/index.js
@@ -12,17 +12,17 @@ const JumbotronPage = () => {
           <MDBJumbotron  style={{ padding: 0 }} id="about-image">
 
           <div>
-
-                <MDBCol className="text-black text-center py-5 px-4 my-8" id="aboutstuff">
-                  <MDBCol  className="py-5">
-                  <h3 id="landing" className="h3-responsive my-5 center text-white font-weight-bold" >
-            “Promise Holdings is a consumer-focused private investment firm created to help high potential companies, and high potential people, reach their fullest Promise.  Our only investment criteria are:(a) Great people, (b) Great ideas, and (c) Great fun. Our <u>Promise</u> to everyone we come into contact with - is that we always work with high integrity, high energy, high impact and high trust."
-          </h3>
-                  
-                   
+                <MDBCol >
+                  <div className="text-black text-center py-5 px-4 my-8" id="aboutstuff">
+                    <MDBCol  className="py-5">
+                    <h3 id="landing" className="h3-responsive my-5 center text-white font-weight-bold" >
+              “Promise Holdings is a consumer-focused private investment firm created to help high potential companies, and high potential people, reach their fullest Promise.  Our only investment criteria are:(a) Great people, (b) Great ideas, and (c) Great fun. Our <u>Promise</u> to everyone we come into contact with - is that we always work with high integrity, high energy, high impact and high trust."
+                    </h3>
                     <br />
                     </MDBCol>
-                    </MDBCol>
+                  </div>
+                </MDBCol>
+
           </div>
 
           </MDBJumbotron>
