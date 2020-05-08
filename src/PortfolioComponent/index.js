7e8a4bb62c37b5d23d7e14a27ed005cb515622da
Filip Tatarnik <Ftatarnik@gmail.com>
2020-05-08 12:35:27 -0500

    added foodbarz, marathon ventures, galexy tech, and chicago switchboard businesses to gordon's company portfolio. coulnd't find higher quality photos

--- src/PortfolioComponent/index.js
+++ src/PortfolioComponent/index.js
@@ -40,14 +40,12 @@ const gridExamplesPage = () => {
       <br/>
       <MDBRow className="d-flex justify-content-center">
         <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
-          <img className="akira" src="https://i.imgur.com/TDMJRVfm.png"/>
+          <img className="akira" src="https://i.imgur.com/sPunKz7.jpg"/>
         </MDBCol>
 
         <MDBCol size="12" sm="6" lg="8">
-        <strong>
-        <h2 className="d-flex justify-content-center">Maya Romanoff</h2>
-        </strong>
-        <p className="d-flex justify-content-center">          Maya Romanoff is the largest manufacturer of handcrafted wallcoverings in the United States. For more than 40 years, their artisans have incorporated glass beads, gold leaf, seashells, wood and stitching right in their Chicago studio.</p> <a href="http://www.mayaromanoff.com/">mayaromanoff.com</a>
+        <h2 className="d-flex justify-content-center">Chicago Switchboard</h2>
+        <p className="d-flex justify-content-center">          Chicago Switchboard, is a leading manufacturer of custom‐engineered electrical power distribution equipment, including switchgear, switchboards and panelboards. The Company has built leading share positions in its target markets and is a preferred supplier to many blue‐chip customers who value Chicago Switchboard’s extensive engineering capabilities, high quality products and outstanding customer service. With its deep product innovation and design capabilities, Chicago Switchboard is focused on providing highly‐engineered, value‐added products to its core end markets – general commercial, institutional, manufacturing, infrastructure and data centers – as well as numerous other diverse industries. The Company has established a leading position in many of its end markets through its short manufacturing lead times, premium product quality and comprehensive support services.</p><a href="https://chiswbd.com/">chiswbd.com</a>
         
         </MDBCol>
       </MDBRow>
@@ -66,6 +64,67 @@ const gridExamplesPage = () => {
         </MDBCol>
       </MDBRow>
       <br/>
+
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
+          <img id="stride" className="akira" src="https://i.imgur.com/K24mJUQ.png"/>
+          <br/>
+        </MDBCol>
+
+        <MDBCol size="12" sm="6" lg="8">
+        <strong>
+        <h2 className="d-flex justify-content-center">Foodbarz</h2>
+        </strong>
+        <p className="d-flex justify-content-center">              FoodBarz is the leading high-end commercial foodservice amenity service to multi-tenant suburban office buildings. The company currently has leases for 10 locations located in large, suburban office buildings in the Chicago area where it is the exclusive food provider.</p> <a href="https://foodbarz.com/">foodbarz.com</a>
+        
+        </MDBCol>
+      </MDBRow>
+      <br/>
+
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
+          <img id="stride" className="akira" src="https://i.imgur.com/azONfyb.jpg"/>
+          <br/>
+        </MDBCol>
+
+        <MDBCol size="12" sm="6" lg="8">
+        <strong>
+        <h2 className="d-flex justify-content-center">Galaxy Technologies</h2>
+        </strong>
+        <p className="d-flex justify-content-center">              Galaxy Technologies, located in Winfield, Kansas near the heart of the Air Capital, is a “best value” business partner to companies around the world with complex design and manufacturing needs. We are a major supplier for the design, manufacturing and assembly of production tooling for the aerospace, defense and plastic markets. Major customers include Boeing, Spirit Aerosystems, Northrop Grumman, Lockheed Martin, Rubbermaid and Coleman</p> <a href="https://galaxytool.com/">galaxytool.com</a>
+        
+        </MDBCol>
+      </MDBRow>
+      <br/>
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
+          <img id="stride" className="akira" src="https://i.imgur.com/rwQWYeC.png"/>
+          <br/>
+        </MDBCol>
+
+        <MDBCol size="12" sm="6" lg="8">
+        <strong>
+        <h2 className="d-flex justify-content-center">Marathon Ventures Inc.</h2>
+        </strong>
+        <p className="d-flex justify-content-center">              Founded in 1915 and based in Bellevue, Nebraska, Marathon Ventures (”MVI”) is a leading manufacturer and marketer of private label baking and snacking nuts, as well as our own brand of snack nuts called Pear’s Snacks.</p> <a href="https://marathonventuresinc.com/">marathonventuresinc.com</a>
+        
+        </MDBCol>
+      </MDBRow>
+      <br/>
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
+          <img className="akira" src="https://i.imgur.com/TDMJRVfm.png"/>
+        </MDBCol>
+
+        <MDBCol size="12" sm="6" lg="8">
+        <strong>
+        <h2 className="d-flex justify-content-center">Maya Romanoff</h2>
+        </strong>
+        <p className="d-flex justify-content-center">          Maya Romanoff is the largest manufacturer of handcrafted wallcoverings in the United States. For more than 40 years, their artisans have incorporated glass beads, gold leaf, seashells, wood and stitching right in their Chicago studio.</p> <a href="http://www.mayaromanoff.com/">mayaromanoff.com</a>
+        
+        </MDBCol>
+      </MDBRow>
+      <br/>
       <MDBRow className="d-flex justify-content-center">
         <MDBCol className="d-flex justify-content-center" size="6" sm="6" lg="4">
           <img id="stride" className="akira" src="https://i.imgur.com/O3LS9qFm.png?1"/>
@@ -80,6 +139,8 @@ const gridExamplesPage = () => {
         
         </MDBCol>
       </MDBRow>
+      <br/>
+      <br/>
     </MDBContainer>
     </div>
   );
